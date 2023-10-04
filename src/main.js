251853e4401cad8144f671458222f96b9af26ecd
18790351502 <12484819+zdstyp@user.noreply.gitee.com>
2023-10-04 17:39:47 +0800

    项目初始化失败

--- src/main.js
+++ src/main.js
@@ -3,4 +3,6 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 引入全局样式文件
+import '@/assets/scss/index.scss'
 createApp(App).use(store).use(router).mount('#app')
